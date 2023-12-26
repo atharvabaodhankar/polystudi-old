8f3a0aaca97ad8315c7f421d748acc316b0cd7f1
AtharvaPC <baodhankaratharva@gmail.com>
2023-12-26 22:46:42 +0530

    added hero

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,7 @@ window.addEventListener("scroll", () => {
 // Menu and Navbar
 
 var menu = document.querySelector(".menu-btn");
-const navBtns = document.querySelectorAll('.nav-btn');
+const navBtns = document.querySelectorAll(".nav-btn");
 var nav = document.querySelector("#nav");
 
 menu.addEventListener("click", () => {
@@ -19,20 +19,64 @@ menu.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
 
-navBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
     menu.classList.remove("active");
     nav.classList.remove("active");
   });
 });
 
-
-Shery.mouseFollower({
-  skew: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 0.2,
-});
+// Shery.mouseFollower({
+//   skew: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 0.2,
+// });
 
 Shery.makeMagnet(".magnet");
 
+// Hero page GSAP
+
+var heroAnimate = gsap.timeline();
+
+
+// Shery.textAnimate(".hero-h1", {
+//   style: 2,
+//   y: 10,
+//   delay: 1,
+//   duration: 2,
+//   ease: "cubic-bezier(0.13, 1, 0.320, 1)",
+//   multiplier: 0.1,
+// });
+
+heroAnimate.from(".hero-div-main h3", {
+  x: -100,
+  delay:.4,
+  duration: 1.3,
+  opacity: 0,
+  stagger:2
+});
+heroAnimate.from(".hero-div-main p", {
+  y: 100,
+  duration: 1.4,
+  opacity: 0,
+  stagger:2
+});
+
+var navAnimate = gsap.timeline();
+
+
+navAnimate.from(".magnet", {
+  y: -100,
+  opacity:0,
+  stagger:0.2,
+  duration:1,
+  delay:2
+})
+navAnimate.from(".nav-ul li", {
+  y: -100,
+  opacity:0,
+  stagger:0.2,
+  duration:1,
+  delay:.2
+})
 
