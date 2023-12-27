847bf77ddfab39e473b5f5048dcde3ad1eff2473
AtharvaPC <baodhankaratharva@gmail.com>
2023-12-27 10:32:07 +0530

    fixed navbar

--- JS/script.js
+++ JS/script.js
@@ -38,45 +38,47 @@ Shery.makeMagnet(".magnet");
 
 var heroAnimate = gsap.timeline();
 
-
-// Shery.textAnimate(".hero-h1", {
-//   style: 2,
-//   y: 10,
-//   delay: 1,
-//   duration: 2,
-//   ease: "cubic-bezier(0.13, 1, 0.320, 1)",
-//   multiplier: 0.1,
-// });
+Shery.textAnimate(".hero-h1", {
+  style: 2,
+  y: 10,
+  delay: 1,
+  duration: 2,
+  ease: "cubic-bezier(0.13, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
 
 heroAnimate.from(".hero-div-main h3", {
   x: -100,
-  delay:.4,
+  delay: 0.4,
   duration: 1.3,
   opacity: 0,
-  stagger:2
+  stagger: 2,
 });
 heroAnimate.from(".hero-div-main p", {
   y: 100,
   duration: 1.4,
   opacity: 0,
-  stagger:2
+  stagger: 2,
 });
 
-var navAnimate = gsap.timeline();
-
-
-navAnimate.from(".magnet", {
+gsap.from(".magnet", {
   y: -100,
-  opacity:0,
-  stagger:0.2,
-  duration:1,
-  delay:2
-})
-navAnimate.from(".nav-ul li", {
-  y: -100,
-  opacity:0,
-  stagger:0.2,
-  duration:1,
-  delay:.2
-})
+  opacity: 0,
+  stagger: 0.2,
+  duration: 1,
+  delay: 0.4,
+});
 
+let MenuAnimateFix = gsap.matchMedia();
+
+MenuAnimateFix.add("(max-width: 500px)", () => {
+  gsap.to(".nav-ul li", {
+    none,
+  });
+});
+
+gsap.from(".nav-ul li", {
+  y: -100,
+  stagger: 0.2,
+  duration: 0.7,
+});
