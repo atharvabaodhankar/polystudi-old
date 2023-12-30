69000e52dd6dece7de3df49fc53fa2c1fa18ae28
AtharvaPC <baodhankaratharva@gmail.com>
2023-12-30 11:56:38 +0530

    added Manuals section

--- JS/script.js
+++ JS/script.js
@@ -200,3 +200,56 @@ gsap.from(".nav-ul li", {
   duration: 0.7,
   delay: 1.8,
 });
+
+// Swiper JS
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // centeredSlides: true,
+  loop: true,
+  fade: "true",
+  speed: 1000,
+  slidesPerGroupSkip: 10,
+  grabCursor: true,
+  autoplayDisableOnInteraction: true,
+  autoplay: {
+    enabled: true,
+    delay: 5000,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+
+    },
+    650: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 60,
+
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 40,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    // dynamicBullets: true,
+  },
+});
+
